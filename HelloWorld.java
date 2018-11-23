597ce202d4fad02b44834070fc2833df7f9835ad
Shekoofeh Poorizadeh <shekoofeh.poorizadeh@gmail.com>
2018-11-23 15:38:12 -0600

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+  // author Shekoofeh Poorizadeh
+  // 11-22-2018
+  public static void main (String[] args){
+    System.out.println ("Hello World!!");
+    System.out.println ("WE started Anly 560 - Late Fall 2018 course!!");
+  }
+}
